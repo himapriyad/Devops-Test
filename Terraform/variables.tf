b1228e659f8df8ef2adf259724c3199502ba71db
himapriyad <61088934+himapriyad@users.noreply.github.com>
2022-08-04 20:16:13 +0100

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,10 @@
-variable "resource_group_name_prefix" {
-  default       = "rg"
-  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "location" {
+  type        = string
+  description = "Default resources location"
 }
 
-variable "resource_group_location" {
-  default = "eastus"
-  description   = "Location of the resource group."
+variable "resource_group_name" {
+  type        = string
+  description = "Storage account name"
 }
+
