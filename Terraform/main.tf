07b1d368bd0c6408d2b1a7cfbe5c7c254bf96907
himapriyad <61088934+himapriyad@users.noreply.github.com>
2022-08-04 20:15:07 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,22 @@
-resource "random_pet" "rg-name" {
-  prefix    = var.resource_group_name_prefix
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name      = random_pet.rg-name.id
-  location  = var.resource_group_location
+data "azurerm_client_config" "current" {}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "tamopstf"    
+        storage_account_name = "tfstatedevops"
+        container_name = "azureterraformbuildartifacts"
+    }
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = var.resource_group_name
+  location = var.location
 }
